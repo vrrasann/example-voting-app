803663482997634bad6c66987c7360a70a6a84ac
Satta <sattaravi@gmail.com>
2016-09-16 23:42:42 -0700

    Create job.groovy

---
+++ result/job.groovy
@@ -0,0 +1,9 @@
+node{
+    
+    docker.withRegistry('https://dtrnodednsgqu3wspddbmle.eastus2.cloudapp.azure.com/', 'ddc-dtr-login') {
+        git 'https://github.com/docker/example-voting-app.git'
+
+        docker.build('admin/com.sysgain.voting-app.result','./result').push('latest')
+    }
+    
+}
